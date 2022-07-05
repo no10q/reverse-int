b4e21ec632c70b2d5e2e4ac4e224a34b0761c972
no10q <nikelodeon85@gmail.com>
2022-07-06 00:15:20 +0300

    feat: end task reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
+    str = n.toString();
+    arrayStr = str.split('');
+    reverseArrayStr = arrayStr.reverse();
+    newStr = reverseArrayStr.join('');
+    result = parseFloat(newStr);
+    return result;
+  }
   
-}
